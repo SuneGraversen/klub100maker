f1aaedb0e286d3bbc84b42b1129b0b948947c71c
Sune Graversen <29074256+SuneGraversen@users.noreply.github.com>
2026-08-19 15:20:37 +0200

    added args

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from pydub import AudioSegment
 
 parser = argparse.ArgumentParser(description="A tool for creating klub100 tracks")
 parser.add_argument("filename", type=str, help="The path to the input file to be parsed")
+#optional fade argument with ms
+parser.add_argument("--fade", type=int, default=700, help="The fade in/out duration in milliseconds (default: 700ms)")
+parser.add_argument("-o", type=str, default="klub100.wav", help="The output filename (default: klub100.wav)")
 args = parser.parse_args()
 if not args.filename:
     print("Error: No filename provided.")
@@ -70,13 +73,22 @@ def merge_songs(sortedSongs):
             start = 1000 * minsectosec(head[3])
             end = 1000 * minsectosec(head[4])
 
-        clips.append(curSong[start:end])
+        clips.append(curSong[start:end].fade_in(args.fade).fade_out(args.fade))
+
+        # check if speak exists, and if it does, add it to clips
+        try:
+            speak_path = f"speaks/{head[0]}_speak.wav"
+            curSpeak = AudioSegment.from_wav(speak_path)
+            clips.append(curSpeak)
+        except FileNotFoundError:
+            print(f"Warning: The speak file '{speak_path}' does not exist. Skipping this speak.")
 
     full_mix = AudioSegment.empty()
+    print("Merging all clips to single track")
     for clip in clips:
         full_mix += clip
-
-    full_mix.export("klub100.wav", format="wav")
+    print("Exporting final track to klub100.wav")
+    full_mix.export(args.out, format="wav")
 
 
 try:
